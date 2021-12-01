df186161f0e5ce1457d6459e4b17d7ba240a48f2
ComputerSmoke <addison@assurancefamily.com>
2021-11-30 18:06:05 -0600

    initial commit

---
+++ scripts/reinvest_kiro.py
@@ -0,0 +1,12 @@
+from brownie import accounts
+from Crypto.Hash import keccak
+import IUniswapV2Router02
+
+def reinvestKIRO():
+    account = accounts.load("met")
+    kiro = "0xb1191f691a355b43542bea9b8847bc73e7abb137"
+    matic = "0x0000000000000000000000000000000000001010"
+    pair = 
+
+def main():
+    reinvestKIRO()
